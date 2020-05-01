1fd2a20605033bf44fdcddb30391b40b93afb40f
Joseph Deng <xdeng9@gmail.com>
2020-05-01 12:27:03 -0700

    Update controller actions

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,4 +1,5 @@
 class Api::VideosController < ApplicationController
+    before_action :require_login, only: [:create, :update, :destroy]
 
     def index
         @videos = Video.all 
@@ -13,10 +14,38 @@ class Api::VideosController < ApplicationController
     end
 
     def create
+        @video = Video.new(video_params)
+        @video.creator_id = current_user.id 
+        @video.view_count = 0
+
+        if @video.save 
+            render :show 
+        else 
+            render json: @video.errors.full_messages, status: 401
+        end
+    end
+
+    def update 
+        @video = Video.find_by(id: params[:id])
+        if @video.update(video_params)
+            render :show 
+        else
+            render json: @video.errors.full_messages, status: 401
+        end
+    end
+
+    def destroy
+        @video = Video.find_by(id: params[:id])
+        if @video.creator_id == current_user.id 
+            @video.destroy
+            render :show 
+        else
+            render json: ['Must be the creator to delete this video'], status: 401 
+        end
     end
 
     private
     def video_params
-        params.require(:video).permit(:title, :description)
+        params.require(:video).permit(:title, :description, :thumbnail, :video)
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     end
 
     def require_login 
-        redirect_to new_session_url unless logged_in?
+        render json: ['Must be logged in'], status: 401 unless logged_in?
     end
 
     def login!(user) 
